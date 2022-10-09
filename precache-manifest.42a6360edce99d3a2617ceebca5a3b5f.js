1c8efeb5475c90b076ca150c931eee3d45e51483
TonyChar3 <tonyc6731@gmail.com>
2022-10-09 10:39:38 -0400

    Updates

---
+++ precache-manifest.42a6360edce99d3a2617ceebca5a3b5f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4148e4a5bd055689bb56",
+    "url": "/React-robofriends/static/css/main.870b4c40.chunk.css"
+  },
+  {
+    "revision": "4148e4a5bd055689bb56",
+    "url": "/React-robofriends/static/js/main.4148e4a5.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/React-robofriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "442743638445b41df6a4",
+    "url": "/React-robofriends/static/css/2.147490bb.chunk.css"
+  },
+  {
+    "revision": "442743638445b41df6a4",
+    "url": "/React-robofriends/static/js/2.44274363.chunk.js"
+  },
+  {
+    "revision": "d93562038b02dde7ad16",
+    "url": "/React-robofriends/static/js/runtime~main.d9356203.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/React-robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "e96b3c9374e739ec157812e21b96edf5",
+    "url": "/React-robofriends/index.html"
+  }
+];
